dd1e074423281cba687633b2fd74df67e50bfbe7
Nelson <tomneo2004@gmail.com>
2020-06-06 15:05:25 +0800

    setup react-router, add node-sass and prettier modules

---
+++ src/components/Router/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Switch, Route } from "react-router-dom";
+
+import Landing from "../../pages/Landing";
+
+function Router() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Landing} />
+    </Switch>
+  );
+}
+
+export default Router;
