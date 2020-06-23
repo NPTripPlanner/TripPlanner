bc030c88c2f5a8d395cd5da7a6c3fad1df78f2bc
Nelson <tomneo2004@gmail.com>
2020-06-23 10:40:35 +0800

    firebase function

---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,61 @@
+import firebase from 'firebase';
+import {auth} from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAuth = auth(firebaseApp);
+
+export const GetCurrentUser = async ()=>{
+    const user = firebaseAuth.currentUser;
+    if(!user) return null;
+
+    return {
+        uid:user.uid,
+        email:user.email,
+        displayName:user.displayName,
+        verified:user.emailVerified,
+    }
+}
+
+export const Logout  = async ()=>{
+    try{
+        await firebaseAuth.signOut();
+        return GetCurrentUser();
+    }
+    catch(err){
+        return err;
+    }
+}
+
+export const SignUpWithEmailAndPassword = async (email, password, displayName)=>{
+    try{
+        await firebaseAuth.createUserWithEmailAndPassword(email, password);
+        const user = firebaseAuth.currentUser;
+        await user.updateProfile({displayName:displayName});
+
+        return await GetCurrentUser();
+    }
+    catch(err){
+        return err;
+    }
+}
+
+export const LoginWithEmailAndPassword = async (email, password)=>{
+    try{
+        await firebaseAuth.signInWithEmailAndPassword(email, password);
+        return await GetCurrentUser();
+    }
+    catch(err){
+        return err;
+    }
+}
