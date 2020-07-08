f5c656bf75dbf249e58de607d1eff9f17a58ae16
Nelson <tomneo2004@gmail.com>
2020-07-08 11:16:34 +0800

    TPSpinner component

---
+++ src/components/TPSpinner/TPSpinner.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {
+    CircularProgress, 
+    Typography
+} from '@material-ui/core';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+const style = {
+    spinner:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
+    }
+}
+
+const TPSpinner = React.forwardRef(({
+    spinTitle,
+    isLoading=false,
+    children
+},ref) => {
+    const classes = makeStyles(style)();
+
+    if(isLoading){
+        return (
+            <div ref={ref} className={classes.spinner}>
+                <CircularProgress color='primary' />
+                <div>
+                    <Typography variant='h6'>{spinTitle}</Typography>
+                </div>
+            </div>
+        );
+    }
+
+    return children;
+});
+
+export default TPSpinner;
