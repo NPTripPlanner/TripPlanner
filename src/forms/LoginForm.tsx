143b997a86602b932756692ba90a3098408b61f8
Nelson <tomneo2004@gmail.com>
2020-08-23 22:40:38 +0800

    LoginForm complete

--- src/forms/LoginForm.tsx
+++ src/forms/LoginForm.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { LoginStart } from "../redux/user/user.actions";
+import { useSelector, useDispatch } from "react-redux";
+import { LoginStart } from "../redux/user/user.actions";
 
 // import TPForm from "../components/TPForm/TPForm";
 // import TPSpinner from "../components/TPSpinner/TPSpinner";
 
-// import { selectLoginFail } from "../redux/user/user.selector";
+import { selectLoginFail } from "../redux/user/user.selector";
 
 // import { loginFormData, loginFormFields } from "./FormData";
 
 import { Button, Typography, CircularProgress } from "@material-ui/core";
 import { makeStyles, createStyles} from '@material-ui/core/styles';
 import {Lock, Email} from '@material-ui/icons';
-import { Formik, Form } from 'formik';
+import { useFormik } from 'formik';
 import InputField from '../components/InputField/InputField';
 import * as Yup from 'yup';
 
@@ -22,45 +22,15 @@ const style = createStyles({
     flexDirection:'column',
     justifyContent:'center',
     alignItems:'center',
+  },
+  error:{
+    color: 'red',
+    textAlign:'center',
   }
 });
 
 const LoginForm = () => {
-  // const [sent, setSent] = React.useState(false);
-  // const loginError = useSelector(selectLoginFail);
-  // const dispatch = useDispatch();
-
-  // const handleFormSubmit = (data) => {
-  //   setSent(true);
-  //   dispatch(LoginStart(data.email, data.password));
-  // };
-
-  
-
-  // if (loginError && sent) setSent(false);
-
-  // return (
-  //   <TPSpinner spinTitle="Logging in..." isLoading={sent}>
-  //     <TPForm
-  //       error={loginError}
-  //       formData={loginFormData}
-  //       formFields={loginFormFields}
-  //       submitButton={
-  //         <Button variant="contained" color="secondary">
-  //           <Typography variant="h6">Login</Typography>
-  //         </Button>
-  //       }
-  //       onSubmit={handleFormSubmit}
-  //     />
-  //   </TPSpinner>
-  // );
-
-  // const handleSubmit = (values, { setSubmitting }) => {
-  //   setTimeout(() => {
-  //     setSubmitting(false);
-  //     alert(JSON.stringify(values, null, 2));
-  //   }, 500);
-  // }
+ 
   const classes = makeStyles(style)();
 
   const validation = ()=>(Yup.object({
@@ -72,63 +42,71 @@ const LoginForm = () => {
     .required('Required')
   }))
 
-  return(
-    <Formik
-    initialValues={{
+  const loginError = useSelector(selectLoginFail);
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues:{
       email: '',
       password: '',
-    }}
-    validationSchema={validation}
-    onSubmit={(_values, { setSubmitting }) => {
-      setTimeout(() => {
-        setSubmitting(false);
-        // alert(JSON.stringify(values, null, 2));
-      }, 3000);
-    }}
+    },
+    validationSchema: validation,
+    onSubmit:(values,{setSubmitting}) => {
+      dispatch(LoginStart(values.email, values.password));
+      setSubmitting(true);
+    },
+  });
+
+  if(loginError && formik.isSubmitting) formik.setSubmitting(false);
+
+  return(
+    <form 
+    className={classes.form}
+    onSubmit={formik.handleSubmit}
     >
-      {
-        (formik)=>(
-          <Form 
-          className={classes.form}
-          onSubmit={formik.handleSubmit}
-          >
-            <InputField
-            id="email"
-            name="email"
-            type="text" 
-            labelText='Email'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disabled={formik.isSubmitting}
-            endAdornment={<Email />}
-            errorText={formik.touched.email && formik.errors.email?formik.errors.email:null}
-            />
-            <InputField 
-            id="password"
-            name="password"
-            type="password" 
-            labelText='Password'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            disabled={formik.isSubmitting}
-            endAdornment={<Lock />}
-            errorText={formik.touched.password && formik.errors.password?formik.errors.password:null}
-            />
-            {
-            formik.isSubmitting?<CircularProgress color='secondary' />:
-            <Button
-              variant="contained"
-              color="secondary"
-              disabled={formik.isSubmitting}
-              onClick={formik.submitForm}
-            >
-              <Typography variant='h5'>Submit</Typography>
-            </Button>
-            }
-          </Form>
-        )
+      {loginError?
+      <div className={classes.error}>
+        <Typography variant='h6'>{loginError}</Typography>
+      </div>
+      :null
       }
-    </Formik>
+      <InputField
+      id="email"
+      name="email"
+      type="text" 
+      labelText='Email'
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      disabled={formik.isSubmitting}
+      defaultValue={formik.values.email}
+      endAdornment={<Email />}
+      errorText={formik.touched.email && formik.errors.email?formik.errors.email:null}
+      />
+      <InputField 
+      id="password"
+      name="password"
+      type="password" 
+      labelText='Password'
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      disabled={formik.isSubmitting}
+      defaultValue={formik.values.password}
+      endAdornment={<Lock />}
+      errorText={formik.touched.password && formik.errors.password?formik.errors.password:null}
+      />
+      {
+      formik.isSubmitting?<CircularProgress color='secondary' />:
+      <Button
+        variant="contained"
+        color="secondary"
+        disabled={formik.isSubmitting}
+        type='submit'
+      >
+        <Typography variant='h5'>Login</Typography>
+      </Button>
+      }
+      </form>
+
   )
 };
 
