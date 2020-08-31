71a2449529c14afcef7fb94a33782b247c983e90
Nelson <tomneo2004@gmail.com>
2020-08-31 15:40:24 +0800

    add rotation for transition

--- src/motions/motions.ts
+++ src/motions/motions.ts
@@ -8,17 +8,20 @@ export const slideInOut = (
     return {
         initial:{
             [direction]:'100%',
+            rotate:-45,
         },
         enter:{
             [direction]:0,
+            rotate:0,
             transition:{
                 [direction]:{ type: "spring", stiffness: stiffness, damping: damping }
             },
         },
         exit:{
             [direction]:'100%',
+            rotate:45,
             transition:{
-                duration:0.5,
+                duration:0.7,
                 backout:[0.43, 0.13, 0.23, 0.96],
             },
         },
