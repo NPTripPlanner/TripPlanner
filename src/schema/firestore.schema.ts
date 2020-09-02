91db37b1081e3fccda12a9f6f1f587d42ccdba89
Nelson <tomneo2004@gmail.com>
2020-09-02 09:29:03 +0800

    add itineraries schema

--- src/schema/firestore.schema.ts
+++ src/schema/firestore.schema.ts
@@ -1,22 +1,33 @@
 import { 
     Collection,
     CustomRepository,
+    SubCollection,
+    ISubCollection,
 } from 'fireorm';
 import ImprovedRepository from './ImprovedRepository';
 
-// export class Metadata {
-//     createAt : Date;
-//     modifyAt : Date;
-// }
+export class Itineraries{
+    id: string;
+    createAt: Date;
+    modifyAt: Date;
+    name:string;
+    startDateUTC: Date;
+    endDateUTC: Date;
+    totalDays: number;
+    tripArchiveId: string;
+}
 
 @Collection('tripArchive')
 export class TripArchive {
     id: string;
-    createAt : Date;
-    modifyAt : Date;
+    createAt: Date;
+    modifyAt: Date;
     ownerId: string;
     name: string;
     tags: Array<string>;
+
+    @SubCollection(Itineraries, 'itineraries')
+    itineraries?:ISubCollection<Itineraries>
 }
 
 
